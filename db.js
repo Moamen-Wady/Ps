2b233488a28feeef49ccaae85d89c6f56cdebfb0
Moamen-Wady <moamen.wady@gmail.com>
2024-06-23 10:19:15 +0300

    first commit

--- db.js
+++
@@ -1,5 +0,0 @@
-[
-    { "Reservations": {}, "num": 1 }, { "Reservations": {}, "num": 2 }, { "Reservations": {}, "num": 3 },
-    { "Reservations": {}, "num": 4 }, { "Reservations": {}, "num": 5 }, { "Reservations": {}, "num": 6 },
-    { "Reservations": {}, "num": 7 }, { "Reservations": {}, "num": 8 }, { "Reservations": {}, "num": 9 }
-]
