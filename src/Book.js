381cbb105ff597522914d2e082323e981e9f5962
Moamen-Wady <moamen.wady@gmail.com>
2024-06-23 10:23:33 +0300

    first commit

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import "./styles.css"
-import { Outlet } from 'react-router-dom'
-import Categories from './Categories'
+import { Outlet, Link } from 'react-router-dom'
 export default function Book( { notify, getResvs, dummy } ) {
     useEffect(
         () => {
@@ -18,7 +17,31 @@ export default function Book( { notify, getResvs, dummy } ) {
             <h1>Welcome, Gamer...</h1>
             <h1><i className='rnb'>Are</i> <i className='rnb'>You</i> <i className='rnb'>Ready</i>?</h1>
             <h3 className='h3'>Choose Your Desired Game & Room</h3>
-            <Categories all={ all } type={ type } typerr={ typerr } />
+            <div className='fulltablecont'>
+                <div className='tabs'>
+                    <div className='tab' onClick={ typerr } slot='ps'
+                        style={ type == 'ps' ? { backgroundColor: 'rgb(0, 255, 13)' } : {} }>
+                        <h2 className='h2' slot='ps'>play station</h2></div>
+                    <div className='tab' onClick={ typerr } slot='pool'
+                        style={ type == 'pool' ? { backgroundColor: 'rgb(0, 255, 13)' } : {} }>
+                        <h2 className='h2' slot='pool'>pool</h2></div>
+                    <div className='tab' onClick={ typerr } slot='ping'
+                        style={ type == 'ping' ? { backgroundColor: 'rgb(0, 255, 13)' } : {} }>
+                        <h2 className='h2' slot='ping'>ping pong</h2></div>
+                </div>
+                <div className='assets'>
+                    { all[ type ]?.sort( ( a, b ) => a.num - b.num ).map( ( item ) => {
+                        return (
+                            <Link to={ '/book/' + type + '/' + item.num } className='asset' key={ item.num }>
+                                <div key={ type.toString() + ' no. ' + item.num } >
+                                    <h4 className='h4'>{ type.toString() + ' no. ' + item.num }</h4>
+                                    <img className='assetimg' src='pst.jpg' alt='' />
+                                </div>
+                            </Link>
+                        )
+                    } ) }
+                </div>
+            </div>
             <Outlet />
 
         </main >
